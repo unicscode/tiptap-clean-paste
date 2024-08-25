23b8daaaa8a2afa31642cdfc5bf7d0f204688709
symkoceila <koceila.lounas@symbioticsec.ai>
2024-08-25 20:32:10 +0200

    replace defaultOptions with addOptions

--- src/clean-paste.ts
+++ src/clean-paste.ts
@@ -4,13 +4,15 @@ import { Plugin, PluginKey } from "@tiptap/pm/state";
 export const CleanPaste = Extension.create({
   name: "cleanPaste",
 
-  defaultOptions: {
-    // - \x20-\x7E includes ASCII printable characters, including digits, letters, and punctuation.
-    // - \u00A0-\u02AF and \u0370-\u03FF include many Latin, Greek, and other letters, including those with diacritics.
-    // - \p{Letter} includes any Unicode letter character from any language.
-    // - \s includes whitespace characters.
-    // - u flag for Unicode mode.
-    regexPattern: /[^\x20-\x7E\u00A0-\u02AF\u0370-\u03FF\p{Letter}\s]/gu,
+  addOptions() {
+    return {
+      // - \x20-\x7E includes ASCII printable characters, including digits, letters, and punctuation.
+      // - \u00A0-\u02AF and \u0370-\u03FF include many Latin, Greek, and other letters, including those with diacritics.
+      // - \p{Letter} includes any Unicode letter character from any language.
+      // - \s includes whitespace characters.
+      // - u flag for Unicode mode.
+      regexPattern: /[^\x20-\x7E\u00A0-\u02AF\u0370-\u03FF\p{Letter}\s]/gu,
+    };
   },
 
   addProseMirrorPlugins() {
